59509f19f53b92e67995e262d2912b8e38e6ff42
netomilfont <netomilfont@hotmail.com>
2022-09-26 01:10:54 -0300

    adiciona botoes de filter

--- nukenzie/src/App.jsx
+++ nukenzie/src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import TextResume from "./components/TextResume";
+import Button from "./components/Button";
+import TitleList from "./components/TitleList";
 
 const App = () => {
   return (
@@ -9,6 +12,19 @@ const App = () => {
         <Header />
         <div className="container__main">
           <Form />
+        </div>
+        <div className="container__list">
+          <div className="container__filter">
+            <TextResume />
+            <div className="container__buttonFilter">
+              <Button />
+              <Button />
+              <Button />
+            </div>
+            <div>
+              <TitleList />
+            </div>
+          </div>
         </div>
       </div>
     </>
