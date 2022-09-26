125472536cc4f4e1acd35230368679bdbfa97ac5
netomilfont <netomilfont@hotmail.com>
2022-09-26 07:09:49 -0300

    adiciona pagina de inicio com SPA

--- nukenzie/src/App.jsx
+++ nukenzie/src/App.jsx
@@ -3,36 +3,59 @@ import { useState } from "react";
 import "./App.css";
 import Cards from "./components/Cards";
 import Header from "./components/Header";
+import Logo from "./components/Logo";
 import Form from "./components/Form";
 import TextResume from "./components/TextResume";
 import Button from "./components/Button";
+import ImgInicio from "./img/illustration.png";
+import LogoInicio from "./img/NuKenzieInicio.png";
 
 const App = () => {
   const [cards, setCards] = useState([]);
+  const [auth, setAuth] = useState(true);
 
   return (
     <>
-      <div className="container">
-        <Header />
-        <div className="container__main">
-          <Form setCards={setCards} />
-          <div className="container__list">
-            <div className="container__filter">
-              <div className="container__allFilter">
-                <TextResume />
-                <div className="container__buttonFilter">
-                  <Button name={"Todos"} />
-                  <Button name={"Entradas"} />
-                  <Button name={"Despesas"} />
+      {auth ? (
+        <div className="container__Inicio">
+          <section className="container__infoApp">
+            <img src={LogoInicio} alt="" />
+            <h1>Centralize o controle das suas finanças</h1>
+            <p>de forma rápida e segura</p>
+            <button onClick={() => setAuth(!auth)}>Entrar</button>
+          </section>
+          <section className="container__imgApp">
+            <img src={ImgInicio} alt="" />
+          </section>
+        </div>
+      ) : (
+        <div className="container">
+          <header>
+            <div className="container__header">
+              <Header />
+              <button onClick={() => setAuth(!auth)}>Inicio</button>
+            </div>
+          </header>
+          <div className="container__main">
+            <Form setCards={setCards} />
+            <div className="container__list">
+              <div className="container__filter">
+                <div className="container__allFilter">
+                  <TextResume />
+                  <div className="container__buttonFilter">
+                    <Button name={"Todos"} />
+                    <Button name={"Entradas"} />
+                    <Button name={"Despesas"} />
+                  </div>
                 </div>
-              </div>
-              <div className="container__listCard">
-                <Cards transations={cards} />
+                <div className="container__listCard">
+                  <Cards transations={cards} />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
--- nukenzie/src/components/Header/index.jsx
+++ nukenzie/src/components/Header/index.jsx
@@ -1,15 +1,11 @@
-import Button from "../Button";
 import Logo from "../Logo";
 import "./styles.css";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container__header">
-        <Logo />
-        <Button name={"Inicio"} />
-      </div>
-    </header>
+    <div>
+      <Logo />
+    </div>
   );
 };
 
